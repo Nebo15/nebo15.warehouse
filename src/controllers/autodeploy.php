6d6e446e2236c6c975d3704bf716131e6d5010e0
Oleh Samorai <oleg.samorai@gmail.com>
2015-08-13 19:11:44 +0300

    final version of autodeployer

--- src/controllers/autodeploy.php
+++ src/controllers/autodeploy.php
@@ -35,12 +35,32 @@ $app->group('/ips', function () use ($app) {
 });
 
 $app->group('/autodeploy', function() use($app) {
-    $app->get('/', function () use ($app) {
-        $request = $app->request->getBody();
-        $app->logger->log($request);
-    });
     $app->post('/', function () use ($app) {
-        $request = $app->request->getBody();
-        $app->logger->log($request);
+        $payload = $app->request->params('payload');
+        $app->logger->log($payload);
+        if (is_string($payload)) {
+            $data = json_decode($payload);
+            if ($data->state == 'passed') {
+                $commit_id = $data->commit;
+                $app->modelIps->setProject($data->repository->name);
+                $app->modelIps->setEnv($data->branch);
+                $app->modelIps->setProject('mbank.api');
+                $app->modelIps->setEnv('develop');
+                $body = json_encode(['project'=>$data->repository->name, 'commit' => $commit_id]);
+                $ips = $app->modelIps->getIps();
+                foreach ($ips as $ip) {
+                    $client = new \Guzzle\Http\Client('http://'.$ip);
+                    $client->setDefaultOption('headers', ['Content-type' => 'application/json']);
+                    $client->setDefaultOption('exceptions', false);
+                    $client->setDefaultOption('timeout', 20);
+                    $client->setDefaultOption('connecttimeout', 0);
+                    $client->setDefaultOption('debug', false);
+                    $request = $client->post("/", [], []);
+                    $request->setBody($body);
+                    $request->send();
+                }
+
+            }
+        }
     });
 });
